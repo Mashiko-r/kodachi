a8bc4496086749fe32be8f2b74d45182c1cc5211
AmakusaShigemi <cskawasaki.k@wingarc.com>
2018-03-07 18:40:18 +0900

    設定たち

--- app/src/main/java/com/example/csmashikor/sacrifice/SettingHomeActivity.java
+++ app/src/main/java/com/example/csmashikor/sacrifice/SettingHomeActivity.java
@@ -2,6 +2,7 @@ package com.example.csmashikor.sacrifice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingHomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SettingHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_home);
+    }
+
+    public void onBackButtonTapped(View view){
+        finish();
     }
 }
