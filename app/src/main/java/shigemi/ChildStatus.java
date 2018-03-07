405e7d098bcadcf84e1a3d51456d06fe2af58bc1
Mashiko-r <csmashiko_r@wingarc.com>
2018-03-07 18:13:43 +0900

    Revert "no message"
    This reverts commit 81f9c52fd2f09b05ecea4031454f629f34af734e.

--- app/src/main/java/shigemi/ChildStatus.java
+++ app/src/main/java/shigemi/ChildStatus.java
@@ -1,31 +1,64 @@
+package Shigemi;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.example.csmashikor.sacrifice.MainActivity;
+
+import java.util.Calendar;
 import java.util.Date;
 
 /**
  * Created by csmashiko.r on 2018/02/28./
+ * アプリ内に登録される子供のデータ
+ * 二人目以降は未対応
  */
 
 public class ChildStatus {
+
+    Context applicationContext = MainActivity.getContextOfApplication();
+    SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+    SharedPreferences.Editor editor = pref.edit();
+
     private String name = "";           //名前
     private Date birth;                 //誕生日
     private int sex = 0;                //0=不明 1=男 2=女
     private String nickName = "";       //ニックネーム
 
-    public ChildStatus getChildStatus(){    //ゲッター
-        ChildStatus CS = new ChildStatus(); //インスタンスを生成
-        CS.name = this.name;
-        CS.birth = this.birth;
-        CS.sex = this.sex;
-        CS.nickName = this.nickName;
-        return CS;                         //ChildStatusを返却
-
+    public String getName(){
+        return pref.getString("CHILD_NAME","未設定");
     }
 
-    public void setChildStatus(ChildStatus CS){ //セッター
-        this.name = CS.name;
-        this.birth = CS.birth;
-        this.sex = CS.sex;
-        this.nickName = CS.nickName;
-    }                                           //返却なし
+    public void setName(String s){
+        editor.putString("CHILD_NAME",s);
+        editor.commit();
+    }
+
+    public int getBirthYear(){
+        return pref.getInt("CHILD_BIRTH_YEAR",2000);
+    }
+    public void setBirthYear(int i){
+        editor.putInt("CHILD_BIRTH_YEAR",i);
+        editor.commit();
+    }
+
+    public int getSex(){
+        return pref.getInt("CHILD_SEX",0);
+    }
+
+    public void setSex(int i){
+        editor.putInt("CHILD_SEX",i);
+        editor.commit();
+    }
+
+    public String getNickName(){
+        return pref.getString("CHILD_NICKNAME","さん");
+    }
+
+    public void setNickName(String s){
+        editor.putString("CHILD_NICKNAME",s);
+        editor.commit();
+    }
 
 }
