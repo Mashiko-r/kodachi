235ae596ac5e062baba15c98d414914e64dc1d3e
AmakusaShigemi <cskawasaki.k@wingarc.com>
2018-03-13 18:36:13 +0900

    カメラ起動できますか？

--- app/src/main/java/com/example/csmashikor/sacrifice/CameraActivity.java
+++ app/src/main/java/com/example/csmashikor/sacrifice/CameraActivity.java
@@ -1,26 +1,51 @@
 package com.example.csmashikor.sacrifice;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.widget.ImageView;
+
 
 public class CameraActivity extends AppCompatActivity {
+
+    private final static int RESULT_CAMERA = 1001;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-    }
 
-    public void tookPic(View view){
         //カメラ起動
-        Intent intent = new Intent();
-        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 1);
+        imageView = findViewById(R.id.imageView);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, RESULT_CAMERA);
     }
 
+
+    protected void onActivityResult(int resultCode, Intent data){
+        if(resultCode == RESULT_CAMERA){
+            Bitmap bitmap;
+            //cancelしたケースも含む
+            if(data.getExtras() == null){
+                Log.d("debug", "cancel ?");
+                return;
+            }
+            else{
+                bitmap = (Bitmap)data.getExtras().get("date");
+                //画像サイズを計測
+                int bmpWidth = bitmap.getWidth();
+                int bmpHeight = bitmap.getHeight();
+                Log.d("debug",String.format("w=%d",bmpWidth));
+                Log.d("debug",String.format("w=%d",bmpHeight));
+            }
+            imageView.setImageBitmap(bitmap);
+        }
+
+    }
 
 }
 
